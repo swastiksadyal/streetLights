b3085d9bef465bb14f3bdfaa46576402b38fd9b7
swastik <31072273+swastiksadyal@users.noreply.github.com>
2022-09-25 10:02:47 +0530

    uno update 1

--- code.ino
+++ code.ino
@@ -1,85 +1,56 @@
-// mainc
-int relay_output[4] = {0, 0, 0, 0}; // pins
-int IR_sensor_input[4] = {0, 0, 0, 0};
+int relay_op[2] =  {0, 0}; // relay outputs
+int IR_lane1[2] = {0, 0}; // lane 1
+int IR_lane2[2] = {0, 0}; // lane 2
 
-int controll[4] = {0, 1, 0, 1};
-
+int lane1 = false;
+int lane2 = false;
 void setup(){
-  // IR sensors setup
-  pinMode(IR_sensor_input[0], INPUT);
-  pinMode(IR_sensor_input[1], INPUT);
-  pinMode(IR_sensor_input[2], INPUT);
-  pinMode(IR_sensor_input[3], INPUT);
-
   //Relay setup
-  pinMode(relay_output[0], OUTPUT);
-  pinMode(relay_output[1], OUTPUT);
-  pinMode(relay_output[2], OUTPUT);
-  pinMode(relay_output[3], OUTPUT);
+  pinMode(relay_op[0], OUTPUT);
+  pinMode(relay_op[1], OUTPUT);
+  pinMode(relay_op[2], OUTPUT);
+  pinMode(relay_op[3], OUTPUT);
+
+  // ir lanes
+  pinMode(IR_lane1[0], INPUT);
+  pinMode(IR_lane1[1], INPUT);
+  pinMode(IR_lane2[0], INPUT);
+  pinMode(IR_lane2[1], INPUT);  
 }
 
 void loop(){
+  int IR01 = digitalRead(IR_lane1[0]);
+  int IR02 = digitalRead(IR_lane1[1]);
+  int IR11 = digitalRead(IR_lane2[0]);
+  int IR12 = digitalRead(IR_lane2[1]);
 
-  // first
-  int IR0 = digitalRead(IR_sensor_input[0]);
-  int IR1 = digitalRead(IR_sensor_input[1]);
-  int IR02 = digitalRead(IR_sensor_input[2]);
-  int IR12 = digitalRead(IR_sensor_input[3]);
-  if ((IR0 == 0) && (IR1 == 1)){
-    controll[0] += 1;
-    delay(100);
-  }else if ((IR0 == 1) && (IR1 == 0) && (controll[0] > 0)){
-    controll[0] -= 1;
-    delay(100);
+  //lane 1
+  if (lane1){
+      if((IR01 == 0) || (IR02 == 0)){
+        lane1 = false;
+        digitalWrite(relay_op[0], LOW);
+        delay(2000);
+      }
+  }else{
+    if((IR01 == 0) || (IR02 == 0)){
+      lane1 = true;
+      digitalWrite(relay_op[0], HIGH);
+      delay(5000);
+    }
   }
-  if ((IR02 == 0) && (IR12 == 1)){
-  controll[1] += 1;
-  delay(100);
-  }else if ((IR02 == 1) && (IR12 == 0) && (controll[1] > 0)){
-    controll[1] -= 1;
-    delay(100);
-  }
-  
 
-  // controolll
-  if (controll[0] > 0){
-    digitalWrite(relay_output[0], HIGH);
-    delay(6000);
+  //lane 2
+  if (lane2){
+    if((IR11 == 0) || (IR12 == 0)){
+      lane2 = false;
+      digitalWrite(relay_op[1], LOW);
+      delay(2000);
+    }
   }else{
-    delay(1000);
-    digitalWrite(relay_output[0], LOW);
-    delay(2000);
-  }
-  if (controll[1] > 0){
-  digitalWrite(relay_output[1], HIGH);
-  delay(6000);
-  }else{
-    delay(1000);
-    digitalWrite(relay_output[1], LOW);
-    delay(2000);
+    if((IR11 == 0) || (IR12 == 0)){
+      lane2 = true;
+      digitalWrite(relay_op[1], HIGH);
+      delay(5000);
+    }
   }
 }
-
-//  int IR_status = digitalRead(IRSensor);
-//
-//  if (IR_status == 1){
-//    delay(3000);
-//    digitalWrite(relay_one, HIGH);
-//
-//    // digitalWrite(8, LOW);
-//  }
-//  
-//
-//
-//
-//}
-  
-//  if (statusSensor == 1)
-//    digitalWrite(LED, LOW); // LED LOW
-//  }
-//  
-//  else
-//  {
-//    digitalWrite(LED, HIGH); // LED High
-//  }
-  
